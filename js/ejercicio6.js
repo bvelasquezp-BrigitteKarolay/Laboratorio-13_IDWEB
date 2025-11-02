2470dd34ac024b4ffa6982b5de33d81aeadf2916
Brigitte Karolay <bvelasquezp@unsa.edu.pe>
2025-11-02 15:42:53 -0500

    Delete js directory

--- js/ejercicio6.js
+++
@@ -1,8 +0,0 @@
-// CREAR UN SET CON NOMBRES Y ELIMINAR DUPLICADOS
-function ejecutarEjercicio6() {
-  const entrada = prompt("Ingrese nombres separados por coma (puede repetir):");
-  const nombres = entrada.split(",").map(nombre => nombre.trim());
-  const conjunto = new Set(nombres);
-  document.getElementById("resultado6").innerText =
-    "Nombres sin duplicados: [" + Array.from(conjunto).join(", ") + "]";
-}
